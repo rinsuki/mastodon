faddf70933dd976c6da5fa22260d9043511bfc5a
masarakki <masaki182@gmail.com>
2017-06-20 02:36:15 +0900

    fix-smlink-tagging

---
+++ spec/friends/process_hashtags_service_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe Friends::ProcessHashtagsService do
+  let(:account) { Fabricate(:account, username: 'alice') }
+  let(:status)  { Fabricate(:status, account: account, text: text) }
+  let(:service) { ProcessHashtagsService.new }
+
+  subject { service.call(status) }
+
+  describe 'single' do
+    let(:text) { 'sm9' }
+    it do
+      subject
+      expect(status.tags.map(&:name)).to include 'smlink'
+    end
+  end
+
+  describe 'double' do
+    let(:text) { 'sm9 sm9' }
+    it do
+      expect { subject }.to change { Tag.count }.by(1)
+      expect(status.tags.map(&:name)).to include 'smlink'
+    end
+  end
+
+  describe 'multi' do
+    let(:text) { 'sm9 lv1' }
+    it do
+      subject
+      status.tags.map(&:name).tap do |tags|
+        expect(tags).to include 'smlink'
+        expect(tags).to include 'lvlink'
+      end
+    end
+  end
+
+  describe 'with string' do
+    let(:text) { 'sm9 #smlink' }
+    it do
+      expect { subject }.to change { Tag.count }.by(1)
+    end
+  end
+end
