37bc3a25c69d05832013732fbce726041aa91bd1
masarakki <masaki182@gmail.com>
2017-09-04 15:46:38 +0900

    ignore-about-pages

--- app/controllers/auth/omniauth_callbacks_controller.rb
+++ app/controllers/auth/omniauth_callbacks_controller.rb
@@ -17,4 +17,26 @@ class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       end
     end
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    last_url = stored_location_for(:user)
+
+    if home_paths(resource).include?(last_url)
+      root_path
+    else
+      last_url || root_path
+    end
+  end
+
+  private
+
+  def home_paths(resource)
+    paths = [about_path]
+    if single_user_mode? && resource.is_a?(User)
+      paths << short_account_path(username: resource.account)
+    end
+    paths
+  end
 end
