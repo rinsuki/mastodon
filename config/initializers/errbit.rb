ac72296bed2c5099cb6ffec88b0a1ae2dac42c46
masarakki <masaki182@gmail.com>
2017-11-27 16:17:01 +0900

    load errbit only ERRBIT_PROJECT_ID present

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,20 +1,23 @@
-require 'rake'
-require 'airbrake'
+if ENV['DISABLE_PROJECT_ID'].present?
 
-IGNORE_EXCEPTIONS = %w(Mastodon::UnexpectedResponseError OpenSSL::SSL::SSLError)
+  require 'rake'
+  require 'airbrake'
 
-Airbrake.configure do |config|
-  config.project_key = ENV['ERRBIT_PROJECT_KEY']
-  config.project_id = ENV['ERRBIT_PROJECT_ID']
-  config.host    = ENV['ERRBIT_HOST']
-  config.environment = Rails.env
-  config.ignore_environments = %w(development test)
-end
+  IGNORE_EXCEPTIONS = %w(Mastodon::UnexpectedResponseError OpenSSL::SSL::SSLError)
 
-Airbrake.add_filter(&:ignore!) unless ENV['ERRBIT_HOST']
+  Airbrake.configure do |config|
+    config.project_key = ENV['ERRBIT_PROJECT_KEY']
+    config.project_id = ENV['ERRBIT_PROJECT_ID']
+    config.host    = ENV['ERRBIT_HOST']
+    config.environment = Rails.env
+    config.ignore_environments = %w(development test)
+  end
 
-Airbrake.add_filter do |notice|
-  if notice[:errors].any? { |error| IGNORE_EXCEPTIONS.include?(error[:type]) }
-    notice.ignore!
+  Airbrake.add_filter(&:ignore!) unless ENV['ERRBIT_HOST']
+
+  Airbrake.add_filter do |notice|
+    if notice[:errors].any? { |error| IGNORE_EXCEPTIONS.include?(error[:type]) }
+      notice.ignore!
+    end
   end
 end
