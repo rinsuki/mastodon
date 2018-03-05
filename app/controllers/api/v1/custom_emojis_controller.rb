bbac02014160667a45659e2450684399a0ab2cb7
masarakki <masaki182@gmail.com>
2018-03-06 04:23:42 +0900

    emoji-create-api

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
 class Api::V1::CustomEmojisController < Api::BaseController
+  include Authorization
   respond_to :json
+
+  before_action :set_custom_emoji, only: [:destroy]
 
   def index
     render json: CustomEmoji.local.where(disabled: false), each_serializer: REST::CustomEmojiSerializer
+  end
+
+  def create
+    authorize :custom_emoji, :create?
+
+    @custom_emoji = CustomEmoji.create!(resource_params)
+    render json: @custom_emoji, serializer: REST::CustomEmojiSerializer
+  end
+
+  def destroy
+    authorize :custom_emoji, :destroy?
+
+    @custom_emoji.destroy!
+    render_empty
+  end
+
+  private
+
+  def resource_params
+    params.require(:custom_emoji).permit(:shortcode, :image, :visible_in_picker)
+  end
+
+  def set_custom_emoji
+    @custom_emoji = CustomEmoji.find(params[:id])
   end
 end
